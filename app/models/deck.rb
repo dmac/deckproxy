1dcd61b2fd198a7032c11c98ead1b3fdeeed2824
Jordan Hurwich <jhurwich@Tardis.(none)>
2011-01-22 16:31:12 -0800

    Fix packs_association table and add a deck view

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,7 +1,14 @@
 class Deck < ActiveRecord::Base
-  validates_presence_of :name
   has_many :packs
   has_many :cards, :through => :packs
+
+  def after_create
+    if !self.name
+      name = "Unnamed Deck (" + self.id.to_s + ")"
+      self.name =name
+      self.save
+    end
+  end
 
   def add_card(card, num = 1)
     pack = Pack.new
@@ -9,5 +16,15 @@ class Deck < ActiveRecord::Base
     pack.deck = self
     pack.card = card
     pack.save
+    # this should increment if found
+  end
+
+  def rename(newName)
+    self.name = newName
+    self.save
+  end
+
+  def self.get_all_decks
+    find(:all, :order => "name")
   end
 end
--- db/migrate/20110122202621_add_decks_table.rb
+++ db/migrate/20110122202621_add_decks_table.rb
@@ -1,5 +1,5 @@
 class AddDecksTable < ActiveRecord::Migration
-  def self.up    
+  def self.up
     create_table :decks do |t|
       t.string :name, :limit => 1024
       t.timestamps
