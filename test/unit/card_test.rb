7771b14e79dac3eb1d3373848dd38163cf99c9dc
Jordan Hurwich <jhurwich@Tardis.(none)>
2011-01-24 08:08:34 -0800

    Add unit test to detect invalid card image urls

--- test/unit/card_test.rb
+++ test/unit/card_test.rb
@@ -1,4 +1,6 @@
 require 'test_helper'
+require "net/http"
+require "uri"
 
 class CardTest < ActiveSupport::TestCase
   # Replace this with your real tests.
@@ -6,8 +8,30 @@ class CardTest < ActiveSupport::TestCase
     assert true
   end
 
+  S3_BASE_URL = "https://s3-us-west-1.amazonaws.com/deckproxy.com/scans/"
+
   test "card images" do
-    puts("*************************\n");
+    cards = Card.all
+    puts("Running card images test (" + cards.size.to_s + " cards to test) \n")
+    cards.each do |card|
+      puts("Validating " + card.name + "\n")
+      url_valid?(S3_BASE_URL + card.edition + "/" + card.number + ".jpg");
+    end
 
   end
+
+  def self.url_valid?(uriStr)
+    uri = URI.parse(uriStr)
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    response = http.request(request)
+    if (response != 200)
+      puts("ERROR! " + uriStr + " didn't work!\n");
+    else
+      puts("y");
+    end
+  end
+
 end
