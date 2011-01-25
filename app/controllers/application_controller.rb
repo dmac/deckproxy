5f87eb4defc231eb8c362ba51c9e9979eade159b
Daniel MacDougall <dmacdougall@gmail.com>
2011-01-25 00:52:20 -0800

    user controller and other files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,27 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  def login_required
+    if session[:user]
+      return true
+    end
+    flash[:warning]="Please login to continue"
+    session[:return_to] = request.request_uri
+    redirect_to :controller => "search", :action => "index"
+    return false
+  end
+
+  def current_user
+    session[:user]
+  end
+
+  def redirect_to_stored
+  if return_to = session[:return_to]
+    session[:return_to] = nil
+    redirect_to_url(return_to)
+  else
+    redirect_to :controller => "search", :action => "index"
+  end
+  end
 end
