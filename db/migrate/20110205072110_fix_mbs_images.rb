6a425e7df2fb37adf87a0a4c76a87e49716ed1ea
Jordan Hurwich <jhurwich@Tardis.(none)>
2011-02-05 00:41:46 -0800

    Attach Mirrodin Besieged to Scars of Mirrodin

--- db/migrate/20110205072110_fix_mbs_images.rb
+++ db/migrate/20110205072110_fix_mbs_images.rb
@@ -2,11 +2,11 @@ class FixMbsImages < ActiveRecord::Migration
   def self.up
 		name_to_correct_num = {
 	 		"Red Sun's Zenith" => 74,
-			'Magnetic Mine' => 114,
-			'Mirrorworks' => 113,
-			'Razorfield Rhino' => 128,
+			'Magnetic Mine' => 113,
+			'Mirrorworks' => 114,
+			'Razorfield Rhino' => 127,
 			'Ogre Resister' => 72,
-			'Rusted Slasher' => 127,
+			'Rusted Slasher' => 128,
 			'Rally the Forces' => 73
 		}
 
