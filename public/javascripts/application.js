0d623a9dac210822e779e173684ed44107514c1f
Jordan Hurwich <jhurwich@Tardis.(none)>
2011-02-14 16:31:18 -0800

    Fix deck metadata to side of page and allow cards to float.  NOTE repositionDeckMetadata should be moved to a general Utils js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+Utils = {
+
+  repositionDeckMetadata: function(minLeft, maxLeft) {
+    var width = parseInt($("#deckmetadata").css("width"));
+    if (window.innerWidth > maxLeft + width) {
+      $("#deckmetadata").css("left", maxLeft);
+    }
+    else if (window.innerWidth > (minLeft + width)) {
+      $("#deckmetadata").css("left", (window.innerWidth - width));
+    }
+    else {
+      $("#deckmetadata").css("left", (minLeft));
+    }
+  }
+}
