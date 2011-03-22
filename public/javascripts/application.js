8a83ba309817afa58e22fa8ad113699cffd4ef7d
mmellenthin <mike.mellenthin@gmail.com>
2011-03-22 14:56:36 -0700

    decksexy draft 1

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-Utils = {
+/*Utils = {
   repositionDeckMetadata: function(minLeft, maxLeft) {
     var width = parseInt($("#deckmetadata").css("width"));
     if (window.innerWidth > maxLeft + width) {
@@ -24,3 +24,4 @@ Utils = {
     });
   }
 }
+*/
